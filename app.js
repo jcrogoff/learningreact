e5e1f77bacddaf66cb323fc4641f88b04ab4686a
Juliana Rogoff <jcrogoff@gmail.com>
2019-06-12 15:45:32 -0400

    adding all files

--- app.js
+++ app.js
@@ -1,105 +1,87 @@
+const players = [  {
+    name: "Guil",
+    score: 50,
+    id: 1
+  },
+  {
+    name: "Treasure",
+    score: 85,
+    id: 2
+  },
+  {
+    name: "Ashley",
+    score: 95,
+    id: 3
+  },
+  {
+    name: "James",
+    score: 80,
+    id: 4
+  }];
+
 const Header = (props) => {
-  return (
-    <header>
-      <span className="stats">Players: {props.totalPlayers}</span> 
-      <h1>{ props.title }</h1>
-    </header>
-  );
+    return (
+        <header>
+            <h1>{props.title}</h1>
+            <span className = "stats">Players: {props.totalPlayers}</span>
+        </header>
+    );
+}
+
+const Player = (props) => {
+    return (
+        <div className = "player">
+            <span className = "player-name">
+                {props.name}
+            </span>
+            <Counter 
+                score = {props.score}
+            />
+        </div>
+    );
 }
 
 class Counter extends React.Component {
-  
-  state = { 
-    score: 100 
-  };
-  
-  incrementScore() {
-    this.setState( prevState => ({ 
-      score: prevState.score + 1 
-    }));
-  }
-  
-  decrementScore() {
-    this.setState( prevState => ({ 
-      score: prevState.score - 1 
-    }));
-  }
+    state = {
+        score: 0
+    };
 
-  render() {
-    return (
-      <div className="counter">
-        <button className="counter-action decrement" onClick={() => this.decrementScore()}> - </button>
-        <span className="counter-score"> {this.state.score} </span>
-        <button className="counter-action increment" onClick={() => this.incrementScore()}> + </button>
-      </div>    
-    );
-  }
-}
-  
-const Player= (props) => {
-  return (
-    <div className="player">
-      <span className="player-name">
-        <button className="remove-player" onClick={() => props.removePlayer(props.id)}>✖</button>
-        { props.name }
-      </span>
+    incrementScore(){
+        console.log('hi')
+    }
 
-      <Counter />
-    </div>
-  );
+    render() {
+        return (
+            <div className = "counter">
+                <button className = "counter-action decrement"> - </button>
+                <span className = "counter-score">{ this.state.score }</span>
+                <button className = "counter-action increment"> + </button>
+            </div>
+
+        );
+    }
 }
 
-class App extends React.Component {
-  
-  state = {
-    players: [
-      {
-        name: "Guil",
-        id: 1,
-      },
-      {
-        name: "Treasure",
-        id: 2,    
-      },
-      {
-        name: "Ashley",
-        id: 3,    
-      },
-      {
-        name: "James",
-        id: 4,    
-      }
-    ]
-  };
+const App = (props) => {
+    return(
+        <div className = "scoreboard">
+            <Header 
+                title="Scoreboard" 
+                totalPlayers = {props.initialPlayers.length}/>
+            {/* Players list */}
+            {props.initialPlayers.map( player =>
+                <Player 
+                    name = {player.name}
+                    key = {player.id.toString()}
+                />
+            )}
+        </div>
         
-  handleRemovePlayer = (id) => {
-    this.setState( prevState => {
-      return {
-        players: prevState.players.filter(p => p.id !== id)
-      }
-    });
-  }
-  
-  render() {
-    return (
-      <div className="scoreboard">
-        <Header 
-          title="My Scoreboard" 
-          totalPlayers={this.state.players.length} 
-        />
-        
-        {/* Players list */}
-        {this.state.players.map( player =>
-          <Player 
-            removePlayer={ this.handleRemovePlayer }      
-            name={player.name}
-            id={player.id}
-            key={player.id.toString()}
-          />
-        )}  
-      </div>
-    );
-  }
-}  
 
-ReactDOM.render(<App />, document.getElementById('root'));
+    );
+}
+
+ReactDOM.render(
+    <App initialPlayers = {players}/>,
+    document.getElementById('root')
+);
